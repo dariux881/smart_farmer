435e77277e3c415557de9b6704691a4a38eae5a8
Dario Di Mauro <dmdario88@gmail.com>
2022-12-23 16:46:00 +0100

    - adding plant code and ids - migrating to net6

--- SmartFarmer.Tests/Utils/InformationLoader.cs
+++ SmartFarmer.Tests/Utils/InformationLoader.cs
@@ -16,24 +16,42 @@ namespace SmartFarmer.Tests.Utils
 
             using (var reader = new StreamReader(filename))
             {
+                int id = 0;
                 while (!reader.EndOfStream)
                 {
                     var line = reader.ReadLine();
+
+                    if (line == null) break;
+                    if (line == string.Empty || line.Trim().StartsWith("#")) continue;
+
                     var tokens = line.Split(",");
+
+                    var code = tokens[0].Trim();
+                    var name = tokens[1].Trim();
+                    var waterAmount = tokens[2].Trim();
+                    var waterTimes = tokens[3].Trim();
+                    var month = tokens[4].Trim();
+                    var weeksToHarvest = tokens[5].Trim();
+                    var width = tokens[6].Trim();
+                    var depth = tokens[7].Trim();
 
                     plants.Add(new FarmerPlant()
                     {
-                        Name = tokens[0],
+                        ID = id + "",
+                        Code = code,
+                        FriendlyName = name,
                         IrrigationInfo = new FarmerIrrigationTaskInfo()
                         {
-                            AmountOfWaterInLitersPerTime = double.Parse(tokens[1], CultureInfo.InvariantCulture),
-                            TimesPerWeek = int.Parse(tokens[2], CultureInfo.InvariantCulture)
+                            AmountOfWaterInLitersPerTime = double.Parse(waterAmount, CultureInfo.InvariantCulture),
+                            TimesPerWeek = int.Parse(waterTimes, CultureInfo.InvariantCulture)
                         },
-                        MonthToPlan = int.Parse(tokens[3], CultureInfo.InvariantCulture),
-                        NumberOfWeeksToHarvest = int.Parse(tokens[4], CultureInfo.InvariantCulture),
-                        PlantWidth = int.Parse(tokens[5], CultureInfo.InvariantCulture),
-                        PlantDepth = int.Parse(tokens[6], CultureInfo.InvariantCulture)
+                        MonthToPlan = int.Parse(month, CultureInfo.InvariantCulture),
+                        NumberOfWeeksToHarvest = int.Parse(weeksToHarvest, CultureInfo.InvariantCulture),
+                        PlantWidth = int.Parse(width, CultureInfo.InvariantCulture),
+                        PlantDepth = int.Parse(depth, CultureInfo.InvariantCulture)
                     });
+
+                    id++;
                 }
             }
 
@@ -48,34 +66,46 @@ namespace SmartFarmer.Tests.Utils
             using var reader = new StreamReader(filename);
 
             Dictionary<string, int> numberByPlant = new Dictionary<string, int>();
+            var id = 0;
+
             while (!reader.EndOfStream)
             {
                 var line = reader.ReadLine();
-                if (line == null) continue;
+                
+                if (line == null) break;
+                if (line == string.Empty || line.Trim().StartsWith("#")) continue;
 
                 var tokens = line.Split(",");
 
-                var plantKind = plants.First(x => x.Name == tokens[1]);
+                var name = tokens[0].Trim();
+                var kind = tokens[1].Trim();
+                var plantedWhen = tokens[2].Trim();
+                var x = tokens[3].Trim();
+                var y = tokens[4].Trim();
 
-                if (!numberByPlant.ContainsKey(plantKind.Name))
+                var plantKind = plants.First(x => x.ID == kind);
+
+                if (!numberByPlant.ContainsKey(plantKind.ID))
                 {
-                    numberByPlant.Add(plantKind.Name, 0);
+                    numberByPlant.Add(plantKind.ID, 0);
                 }
 
-                var plantName = tokens[0];
-                if (string.IsNullOrEmpty(plantName))
+                if (string.IsNullOrEmpty(name))
                 {
-                    plantName = plantKind.Name + numberByPlant[plantKind.Name];
-                    numberByPlant[plantKind.Name]++;
+                    name = plantKind.FriendlyName + numberByPlant[plantKind.ID];
+                    numberByPlant[plantKind.ID]++;
                 }
 
-                plantsInstance.Add(new FarmerPlantInstance(plants.First(x => x.Name == tokens[1]))
+                plantsInstance.Add(new FarmerPlantInstance(plantKind)
                 {
-                    PlantName = plantName,
-                    PlantedWhen = string.IsNullOrEmpty(tokens[2]) ? DateTime.UtcNow : Convert.ToDateTime(tokens[2]),
-                    PlantX = int.Parse(tokens[3], CultureInfo.InvariantCulture),
-                    PlantY = int.Parse(tokens[4], CultureInfo.InvariantCulture)
+                    ID = id + "",
+                    PlantName = name,
+                    PlantedWhen = string.IsNullOrEmpty(plantedWhen) ? DateTime.UtcNow : Convert.ToDateTime(plantedWhen),
+                    PlantX = int.Parse(x, CultureInfo.InvariantCulture),
+                    PlantY = int.Parse(y, CultureInfo.InvariantCulture)
                 });
+
+                id++;
             }
 
             return plantsInstance;
