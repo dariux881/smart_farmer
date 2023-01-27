5d05af0e2b4c5c49be2357032a649192ea3fec0c
dariux881 <dmdario88@gmail.com>
2023-01-27 13:50:02 +0100

    improving webapp and controllers

--- SmartFarmer/Data/SmartFarmerDbContext.cs
+++ SmartFarmer/Data/SmartFarmerDbContext.cs
@@ -1,11 +1,36 @@
-using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
 using SmartFarmer.DTOs;
 using SmartFarmer.DTOs.Alerts;
+using SmartFarmer.DTOs.Plants;
+using SmartFarmer.DTOs.Security;
+using SmartFarmer.DTOs.Tasks;
 
 namespace SmartFarmer.Data;
 
 public class SmartFarmerDbContext : DbContext
 {
+    public SmartFarmerDbContext(DbContextOptions<SmartFarmerDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<FarmerPlant>()
+            .Property(f => f.IrrigationInfo).
+            .HasColumnType<FarmerIrrigationTaskInfo>(new PropertyBuilder());
+
+    }
+
     public DbSet<FarmerGround> Grounds { get; set; }
+    public DbSet<FarmerPlant> Plants { get; set; }
+    public DbSet<FarmerPlantInstance> PlantsInstance { get; set; }
     public DbSet<FarmerAlert> Alerts { get; set; }
+    // public DbSet<FarmerPlan> PlantsInstance { get; set; }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserLogin> Logins { get; set; }
+
 }
