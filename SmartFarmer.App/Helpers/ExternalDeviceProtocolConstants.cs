532d0252c920d59cff13382728b3e2601eaaca7a
dariux881 <dmdario88@gmail.com>
2023-04-20 16:08:14 +0200

    preparing new commands

--- SmartFarmer.App/Helpers/ExternalDeviceProtocolConstants.cs
+++ SmartFarmer.App/Helpers/ExternalDeviceProtocolConstants.cs
@@ -3,9 +3,13 @@ namespace SmartFarmer.Helpers;
 public class ExternalDeviceProtocolConstants
 {
     public const string STOP = "STP";
+    public const string SLEEP = "SLP";
     public const string MOVE_XY_COMMAND = "MXY";
     public const string MOVE_TO_HEIGHT_COMMAND = "MHT";
     public const string MOVE_TO_MAX_HEIGHT_COMMAND = "MMH";
     public const string TURN_HORIZONTAL_COMMAND = "THZ";
     public const string TURN_VERTICAL_COMMAND = "TVT";
+    public const string HANDLE_PUMP_COMMAND = "PMP";
+
+    public const int    INVALID_PARAMETERS = -1;
 }
