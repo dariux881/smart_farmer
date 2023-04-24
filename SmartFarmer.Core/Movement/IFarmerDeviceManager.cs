dfc199086f56e7f878c8512ab280959eeada7572
dariux881 <dmdario88@gmail.com>
2023-04-24 10:24:50 +0200

    renaming tasks - introducing partial result

--- SmartFarmer.Core/Movement/IFarmerDeviceManager.cs
+++ SmartFarmer.Core/Movement/IFarmerDeviceManager.cs
@@ -6,6 +6,7 @@ public interface IFarmerDeviceManager :
     IFarmerMoveOnGridDevice,
     IFarmerMoveAtHeightDevice,
     IFarmerTurnToolDevice,
+    IFarmerWaterProviderDevice,
     IFarmer5dPointNotifier
 {
 
