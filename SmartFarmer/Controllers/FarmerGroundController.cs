a5fd306c8aabf0afbb9bbad04bed9a34a860db1d
dariux881 <dmdario88@gmail.com>
2023-05-23 16:02:51 +0200

    separating controllers with different scopes

--- SmartFarmer/Controllers/FarmerGroundController.cs
+++ SmartFarmer/Controllers/FarmerGroundController.cs
@@ -1,38 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using SmartFarmer.Alerts;
 using SmartFarmer.Plants;
 using SmartFarmer.Helpers;
 using SmartFarmer.Services;
-using SmartFarmer.Tasks.Generic;
 using SmartFarmer.DTOs.Plants;
 using SmartFarmer.Movement;
 using SmartFarmer.DTOs.Movements;
-using SmartFarmer.Tasks;
 
 namespace SmartFarmer.Controllers;
 
 [ApiController]
 [Route("[controller]")]
 [Authorize]
-public class FarmerGroundController : ControllerBase
+public class FarmerGroundController : FarmerControllerBase
 {
     private readonly ILogger<FarmerGroundController> _logger;
     private readonly ISmartFarmerGroundControllerService _groundProvider;
-    private readonly ISmartFarmerUserAuthenticationService _userManager;
 
     public FarmerGroundController(
         ILogger<FarmerGroundController> logger,
         ISmartFarmerGroundControllerService groundProvider,
         ISmartFarmerUserAuthenticationService userManager)
+        : base(userManager)
     {
         _logger = logger;
         _groundProvider = groundProvider;
-        _userManager = userManager;
     }
 
     [HttpGet]
@@ -157,147 +152,6 @@ public class FarmerGroundController : ControllerBase
         return Ok(plant);
     }
 
-    [HttpGet("plan")]
-    [IsUserAuthorizedTo(Constants.AUTH_READ_GROUND)]
-    public async Task<ActionResult<IFarmerPlan>> GetPlan(string id)
-    {
-        var userId = await GetUserIdByContext();
-
-        if (string.IsNullOrEmpty(userId))
-            return Unauthorized();
-
-        var plan =
-            await _groundProvider
-                .GetFarmerPlanByIdForUserAsync(userId, id);
-
-        return Ok(plan);
-    }
-
-    [HttpGet("plans")]
-    [IsUserAuthorizedTo(Constants.AUTH_READ_GROUND)]
-    public async Task<ActionResult<IEnumerable<IFarmerPlant>>> GetPlans(string ids)
-    {
-        var userId = await GetUserIdByContext();
-
-        if (string.IsNullOrEmpty(userId))
-            return Unauthorized();
-
-        var plans =
-            await _groundProvider
-                .GetFarmerPlanByIdsForUserAsync(userId, ids.Split('#'));
-
-        return Ok(plans);
-    }
-
-    [HttpGet("steps")]
-    [IsUserAuthorizedTo(Constants.AUTH_READ_GROUND)]
-    public async Task<ActionResult<IEnumerable<IFarmerPlanStep>>> GetSteps(string ids)
-    {
-        var steps =
-            await _groundProvider
-                .GetFarmerPlanStepByIdsAsync(ids.Split('#'));
-
-        return Ok(steps);
-    }
-
-    [HttpGet("alertsInGround")]
-    [IsUserAuthorizedTo(Constants.AUTH_READ_GROUND)]
-    public async Task<ActionResult<IEnumerable<IFarmerAlert>>> GetAlertsByGround(string id)
-    {
-        var userId = await GetUserIdByContext();
-
-        if (string.IsNullOrEmpty(userId))
-            return Unauthorized();
-
-        var alerts =
-            await _groundProvider
-                .GetFarmerAlertsByGroundIdAsync(userId, id);
-
-        return Ok(alerts);
-    }
-
-    [HttpGet("alertsCountInGround")]
-    [IsUserAuthorizedTo(Constants.AUTH_READ_GROUND)]
-    public async Task<ActionResult<int>> GetAlertsCountByGround(string id)
-    {
-        var userId = await GetUserIdByContext();
-
-        if (string.IsNullOrEmpty(userId))
-            return Unauthorized();
-
-        var alerts =
-            (await _groundProvider
-                .GetFarmerAlertsByGroundIdAsync(userId, id))?.ToArray();
-
-        return Ok(alerts?.Length);
-    }
-
-    [HttpGet("alertsCountToReadInGround")]
-    [IsUserAuthorizedTo(Constants.AUTH_READ_GROUND)]
-    public async Task<ActionResult<IEnumerable<IFarmerAlert>>> GetAlertsToReadCountByGround(string id)
-    {
-        var userId = await GetUserIdByContext();
-
-        if (string.IsNullOrEmpty(userId))
-            return Unauthorized();
-
-        var alerts =
-            (await _groundProvider
-                .GetFarmerAlertsByGroundIdAsync(userId, id))?
-                    .Where(a => !a.MarkedAsRead)
-                    .ToArray();
-
-        return Ok(alerts?.Length);
-    }
-
-    [HttpGet("alerts")]
-    [IsUserAuthorizedTo(Constants.AUTH_READ_GROUND)]
-    public async Task<ActionResult<IEnumerable<IFarmerAlert>>> GetAlertsByIds(string ids)
-    {
-        var userId = await GetUserIdByContext();
-
-        if (string.IsNullOrEmpty(userId))
-            return Unauthorized();
-
-        var alerts =
-            await _groundProvider
-                .GetFarmerAlertsByIdAsync(userId, ids.Split("#"));
-
-        return Ok(alerts);
-    }
-
-    [HttpGet("markAlert")]
-    [IsUserAuthorizedTo(Constants.AUTH_READ_GROUND)]
-    public async Task<ActionResult<bool>> MarkAlertAsRead(string alertId, bool read)
-    {
-        var userId = await GetUserIdByContext();
-
-        if (string.IsNullOrEmpty(userId))
-            return Unauthorized();
-
-        var result =
-            await _groundProvider
-                .MarkFarmerAlertAsRead(userId, alertId, read);
-
-        return Ok(result);
-    }
-
-    [HttpPost("createAlert")]
-    [IsUserAuthorizedTo(Constants.AUTH_EDIT_GROUND)]
-    public async Task<ActionResult<string>> CreateAlert([FromBody]FarmerAlertRequestData data)
-    {
-        var userId = await GetUserIdByContext();
-
-        if (string.IsNullOrEmpty(userId))
-            return Unauthorized();
-
-        var result =
-            await _groundProvider
-                .CreateFarmerAlert(userId, data);
-
-        return Ok(result);
-    }
-
     [HttpPost("createGround")]
     [IsUserAuthorizedTo(Constants.AUTH_EDIT_GROUND)]
     public async Task<ActionResult<string>> CreateGround([FromBody] FarmerGroundRequestData data)
@@ -339,52 +193,6 @@ public class FarmerGroundController : ControllerBase
             .AddFarmerPlantInstance(userId, data);
 
         if (result)
-            return Ok(result);
-
-        return BadRequest(result);
-    }
-
-    [HttpPost("createPlan")]
-    [IsUserAuthorizedTo(Constants.AUTH_EDIT_GROUND)]
-    public async Task<ActionResult<string>> CreateIrrigationPlan([FromBody] FarmerPlanRequestData plan)
-    {
-        if (plan == null)
-        {
-            throw new ArgumentNullException(nameof(plan));
-        }
-
-        var userId = await GetUserIdByContext();
-
-        if (string.IsNullOrEmpty(userId))
-            return Unauthorized();
-
-        var result = await _groundProvider
-            .AddPlan(userId, plan);
-
-        if (!string.IsNullOrEmpty(result))
-            return Ok(result);
-
-        return BadRequest(result);
-    }
-
-    [HttpPost("createIrrigationPlan")]
-    [IsUserAuthorizedTo(Constants.AUTH_EDIT_GROUND)]
-    public async Task<ActionResult<string>> CreateIrrigationPlan(string groundId)
-    {
-        if (string.IsNullOrEmpty(groundId))
-        {
-            throw new ArgumentNullException(nameof(groundId));
-        }
-
-        var userId = await GetUserIdByContext();
-
-        if (string.IsNullOrEmpty(userId))
-            return Unauthorized();
-
-        var result = await _groundProvider
-            .BuildIrrigationPlan(userId, groundId);
-
-        if (!string.IsNullOrEmpty(result))
             return Ok(result);
 
         return BadRequest(result);
@@ -438,15 +246,5 @@ public class FarmerGroundController : ControllerBase
         }
 
         return Ok(result);
-    }
-
-    private async Task<string> GetUserIdByContext()
-    {
-        var token = (string)HttpContext.Items[Constants.HEADER_AUTHENTICATION_TOKEN];
-
-        if (string.IsNullOrEmpty(token))
-            return null;
-
-        return await _userManager.GetLoggedUserIdByToken(token);
     }
 }
